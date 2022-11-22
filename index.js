663317d3dd65fff11b083a328f45e4d000778f53
collins-ruto <collinsruto48@gmail.com>
2022-11-23 00:11:12 +0300

    initial commit

--- index.js
+++ index.js
@@ -55,3 +55,17 @@ portfolio.forEach(item => {
     	idesc.classList.remove("desc-off");
 	});
 })
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		console.log(entry)
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show')
+		} else {
+			entry.target.classList.remove("show");
+		}
+	})
+})
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el))
