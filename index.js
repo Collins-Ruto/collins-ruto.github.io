027d519dd875d08afa109b14c13914af66a2c4dd
collins-ruto <collinsruto48@gmail.com>
2023-05-08 15:58:15 +0300

    resume

--- index.js
+++ index.js
@@ -1,70 +1,47 @@
-let navToggle = document.querySelector('.nav-toggle');
-let navLinks = document.querySelectorAll('.nav__link');
-let boddy = document.querySelector(".bodyy");
-let contactBtn = document.querySelector(".butn1");
-let closer = document.querySelector(".close");
-let contact = document.getElementById("contact")
-let portDesc = document.querySelector(".port-desc");
-let portImg = document.querySelector(".portfolio__item");
-let pdesc = document.getElementById("pdesc");
-let portfolio = document.querySelectorAll(".portfolio__item");
-
-document.querySelectorAll(".anim").forEach(element => {
-	element.classList.add("slow");	
-})
-
-navToggle.addEventListener("click", () => {
-	document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => { 
-	link.addEventListener("click", () => {
-		document.body.classList.remove('nav-open');
-	})
-});
-
-boddy.addEventListener("click", () => {
-	document.body.classList.remove('nav-open');
-});
-
-contactBtn.addEventListener("click", () => {
-	document.body.classList.toggle("contact-open");
-});
-
-closer.addEventListener("click", () => {
-	document.body.classList.remove("contact-open");
-});
-
-portDesc.addEventListener("mouseover", () => {
-	document.body.classList.remove("port-desc");
-	
-})
-
-portfolio.forEach(item => {
-	item.addEventListener("mouseover", () => {
-		let idesc = item.querySelector(".descp");
-		idesc.classList.add("desc-off");
-		console.log("got here")
-	});
-})
-
-portfolio.forEach(item => {
-	item.addEventListener("mouseout", () => {
-		pdesc.classList.remove("desc-off");
-		let idesc = item.querySelector(".descp");
-    	idesc.classList.remove("desc-off");
-	});
-})
-
-const observer = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
-		if (entry.isIntersecting) {
-			entry.target.classList.add('show')
-		} else {
-			entry.target.classList.remove("show");
-		}
-	})
-})
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el))
+let navToggle = document.querySelector(".nav-toggle"),
+  navLinks = document.querySelectorAll(".nav__link"),
+  boddy = document.querySelector(".bodyy"),
+  closer = document.querySelector(".close"),
+  contact = document.getElementById("contact"),
+  portDesc = document.querySelector(".port-desc"),
+  portImg = document.querySelector(".portfolio__item"),
+  pdesc = document.getElementById("pdesc"),
+  portfolio = document.querySelectorAll(".portfolio__item");
+document.querySelectorAll(".anim").forEach((e) => {
+  e.classList.add("slow");
+}),
+  navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
+  }),
+  navLinks.forEach((e) => {
+    e.addEventListener("click", () => {
+      document.body.classList.remove("nav-open");
+    });
+  }),
+  boddy.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  }),
+  portDesc.addEventListener("mouseover", () => {
+    document.body.classList.remove("port-desc");
+  }),
+  portfolio.forEach((e) => {
+    e.addEventListener("mouseover", () => {
+      e.querySelector(".descp").classList.add("desc-off"),
+        console.log("got here");
+    });
+  }),
+  portfolio.forEach((e) => {
+    e.addEventListener("mouseout", () => {
+      pdesc.classList.remove("desc-off");
+      e.querySelector(".descp").classList.remove("desc-off");
+    });
+  });
+const observer = new IntersectionObserver((e) => {
+    e.forEach((e) => {
+      e.isIntersecting
+        ? e.target.classList.add("show")
+        : e.target.classList.remove("show");
+    });
+  }),
+  hiddenElements = document.querySelectorAll(".to_hide");
+hiddenElements.forEach((e) => observer.observe(e));
---
+++ index2.js
@@ -0,0 +1,70 @@
+let navToggle = document.querySelector('.nav-toggle');
+let navLinks = document.querySelectorAll('.nav__link');
+let boddy = document.querySelector(".bodyy");
+// let contactBtn = document.querySelector(".butn1");
+let closer = document.querySelector(".close");
+let contact = document.getElementById("contact")
+let portDesc = document.querySelector(".port-desc");
+let portImg = document.querySelector(".portfolio__item");
+let pdesc = document.getElementById("pdesc");
+let portfolio = document.querySelectorAll(".portfolio__item");
+
+document.querySelectorAll(".anim").forEach(element => {
+	element.classList.add("slow");	
+})
+
+navToggle.addEventListener("click", () => {
+	document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => { 
+	link.addEventListener("click", () => {
+		document.body.classList.remove('nav-open');
+	})
+});
+
+boddy.addEventListener("click", () => {
+	document.body.classList.remove('nav-open');
+});
+
+// contactBtn.addEventListener("click", () => {
+// 	document.body.classList.toggle("contact-open");
+// });
+
+// closer.addEventListener("click", () => {
+// 	document.body.classList.remove("contact-open");
+// });
+
+portDesc.addEventListener("mouseover", () => {
+	document.body.classList.remove("port-desc");
+	
+})
+
+portfolio.forEach(item => {
+	item.addEventListener("mouseover", () => {
+		let idesc = item.querySelector(".descp");
+		idesc.classList.add("desc-off");
+		console.log("got here")
+	});
+})
+
+portfolio.forEach(item => {
+	item.addEventListener("mouseout", () => {
+		pdesc.classList.remove("desc-off");
+		let idesc = item.querySelector(".descp");
+    	idesc.classList.remove("desc-off");
+	});
+})
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show')
+		} else {
+			entry.target.classList.remove("show");
+		}
+	})
+})
+
+const hiddenElements = document.querySelectorAll(".to_hide");
+hiddenElements.forEach((el) => observer.observe(el))
